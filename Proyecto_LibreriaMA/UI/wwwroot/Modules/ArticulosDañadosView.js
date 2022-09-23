9533abd988408f3f1b55e5767959a06b46a32d99
FranciscoRodriguez44 <frankrodrigu6@gmail.com>
2022-09-23 15:28:43 -0600

    inicio de Articulos Dañados

---
+++ Proyecto_LibreriaMA/UI/wwwroot/Modules/ArticulosDañadosView.js
@@ -0,0 +1,6 @@
+import { TableComponent } from "../../CoreComponents/TableComponent.js";
+import { ViewArticuloCompra, ViewCompra } from "../../Model/ViewDatabaseModel.js";
+import { AjaxTools, Render } from "../utility.js";
+
+
+window.onload = async () => {}
