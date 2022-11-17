7cf4a0b00d8673621aa49c488a0104f3f29362c0
Kevinhuete <kevin.mhc25@gmail.com>
2022-11-17 15:36:47 -0600

    informearticulo

---
+++ Proyecto_LibreriaMA/UI/Controllers/InformesArticuloController.cs
@@ -0,0 +1,22 @@
+﻿using CAPA_NEGOCIO.MODEL;
+using CAPA_NEGOCIO.SECURITY;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace UI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InformesArticuloController : Controller
+    {
+       public InformesArticuloController()
+        {
+            AuthNetCore.loginIN("sa", "1234");
+        }
+        [HttpPost]
+        public object TakeInformeArticuloEjemplo1(InformeArticuloEjemplo1 ObjInst)
+        {
+            return ObjInst.TakeInformeArticulo(ObjInst);
+        }
+    }
+}
