618b3e7be41555f1a8e06673ec5263c38829b9ee
Jgabriel19204 <jgabriel19204@gmail.com>
2022-11-21 10:53:16 -0600

    Avance

--- Proyecto_LibreriaMA/UI/wwwroot/Modules/GestioVentas/ModalViewRefactura.js
+++ Proyecto_LibreriaMA/UI/wwwroot/Modules/GestioVentas/ModalViewRefactura.js
@@ -11,11 +11,11 @@ import { AgregarDetalleVenta } from "./AgregarDetalleVenta.js";
 class Agregar extends HTMLElement {
     constructor(action = () => { }) {
         super();
-        this.Dataset =[];
+        this.Dataset = [];
         this.Devolucionventalist = [];
 
         this.action = action;
-        
+
         this.Facturas = FacturaDevolucion;
         this.Detalles = [];
         this.NewDevolucionVenta = {};
@@ -37,33 +37,33 @@ class Agregar extends HTMLElement {
 
     connectedCallback() { }
     Draw = async () => {
-        console.log(this.NewDevolucionVenta.Updateventa );
+        console.log(this.NewDevolucionVenta.Updateventa);
         this.Dataset = await AjaxTools.PostRequest("../api/GestionVenta/ChargeDetaDevVenta");
         this.Devolucionventalist = await AjaxTools.PostRequest("../api/GestionVenta/DevolucionVentaList");
-        this.last = this.Devolucionventalist[this.Devolucionventalist.length-1];
+        this.last = this.Devolucionventalist[this.Devolucionventalist.length - 1];
         console.log("this.last");
 
         console.log(this.last);
         console.log(this.Devolucionventalist);
         console.log(this.Facturas);
-        this.NuevaFactura.nombrecliente = this.Facturas.nombrecliente 
-        this.NuevaFactura.idusuario = this.Facturas.idusuario 
+        this.NuevaFactura.nombrecliente = this.Facturas.nombrecliente
+        this.NuevaFactura.idusuario = this.Facturas.idusuario
         this.DetalleDevventa.iddevolucionventa = this.last.iddevolucionventa
         console.log(this.Facturas);
         this.Form = new FormComponet({
             Model: new Factura({
-                idusuario: {hidden: true}
+                idusuario: { hidden: true }
             }),
-            EditObject: this.NuevaFactura 
+            EditObject: this.NuevaFactura
         }),
-        this.Form12 = new FormComponet({
-            Model: new DetalleDevolucionVenta({
+            this.Form12 = new FormComponet({
+                Model: new DetalleDevolucionVenta({
 
+                }),
+                EditObject: this.DetalleDevventa
             }),
-            EditObject: this.DetalleDevventa
-        }),
 
-        
+
             this.Table = new TableComponent({
                 ModelObject: new DetalleFactura(),
                 Dataset: this.Dataset.filter((factura) => {
@@ -82,26 +82,36 @@ class Agregar extends HTMLElement {
                         action: async (Dato) => {
                             console.log(Dato);
                             this.DetalleDevventa.AdminMercas = []
-                                
-                           this.DetalleDevventa.AdminMercas.push(Dato)
-                            this.DetalleDevventa.cantidad = this.DetalleDevventa.AdminMercas[0].cantidadventa 
-                            this.DetalleDevventa.idadmimercancias =this.DetalleDevventa.AdminMercas[0].idadmimercancias
-                           // this.DetalleDevventa.AdminMercas[0].idadmimercancias= this.DetalleDevventa.idadmimercancias 
-                            this.DetalleDevventa.AdminMercas[0].existenciasarticulounidad =  parseInt(this.DetalleDevventa.cantidad + this.DetalleDevventa.AdminMercas[0].existenciasarticulounidad )
+
+                            this.DetalleDevventa.AdminMercas.push(Dato)
+                            this.DetalleDevventa.cantidad = this.DetalleDevventa.AdminMercas[0].cantidadventa
+                            this.DetalleDevventa.idadmimercancias = this.DetalleDevventa.AdminMercas[0].idadmimercancias
+                            // this.DetalleDevventa.AdminMercas[0].idadmimercancias= this.DetalleDevventa.idadmimercancias 
+                            this.DetalleDevventa.AdminMercas[0].existenciasarticulounidad = parseInt(this.DetalleDevventa.cantidad + this.DetalleDevventa.AdminMercas[0].existenciasarticulounidad)
                             console.log(this.NewDevolucionVenta);
-                           // this.NewDevolucionVenta.DetalleDevventas.pushthis.DetalleDevventa(this.DetalleDevventa)
+                            // this.NewDevolucionVenta.DetalleDevventas.pushthis.DetalleDevventa(this.DetalleDevventa)
+
                             
-                            const Datof = this.Detalles.find((x) => x.iddetallefactura == Dato.iddetallefactura);
+
+                            var resultado = window.confirm('Estas seguro?');
+                            if (resultado === true) {
+                                await AjaxTools.PostRequest("../api/GestionVenta/SaveDetalleDevolucionventa",
+                                    this.NewDevolucionVenta
+                                );
+                                const Datof = this.Detalles.find((x) => x.iddetallefactura == Dato.iddetallefactura);
                             if (Datof != null) {
                                 this.Detalles.splice(this.Detalles.indexOf(Datof), 1);
                                 this.Table.DrawTableComponent(this.Detalles);
                             }
-                            
-                           //console.log(this.DetalleDevventa);
-                            await AjaxTools.PostRequest("../api/GestionVenta/SaveDetalleDevolucionventa",
-                            this.NewDevolucionVenta
-                             );
-                           console.log(this.NewDevolucionVenta);
+                            } else {
+                                this.Table.DrawTableComponent(this.Detalles);
+
+                            }
+                            //console.log(this.DetalleDevventa);
+                            // await AjaxTools.PostRequest("../api/GestionVenta/SaveDetalleDevolucionventa",
+                            // this.NewDevolucionVenta
+                            //  );
+                            console.log(this.NewDevolucionVenta);
                         },
                     },
                 ],
@@ -132,41 +142,41 @@ class Agregar extends HTMLElement {
         //     }
 
         // }))
-        
-     this.TableDetalleVenta = new TableComponent({
-        ModelObject: new DetalleFactura(),
-        Dataset: this.DetallVenta,
-        Functions: [
-            {
-                name: "eliminar", action: async (detaeli) => {
-                    const detalleelimina = this.DetallVenta.find(x => x.idtamanoxarticulo == detaeli.idtamanoxarticulo)
-                    if (detalleelimina != null) {
-                        // this.DetallVenta.splice(
-                        //     DetallVenta.indexOf(detaeli), 1);
-                        //     Total.splice(DetallVenta.indexOf(detaeli.totaldetalle),1);
-                        //     Total.forEach(function(total){
-                        //         var sum2=0
-                        //         suma +=total
-                        //        suma = suma + Total
-                        //         TotalSuma = Total.reduce((a, b) => Number(a) + Number(b), 0);
-                        //         iva = TotalSuma * 0.15;
-                        //         sum2 =TotalSuma
-                        //        totalventa = TotalSuma + iva
-                        //        ///
-                        //       NuevaFactura.totalventa = totalventa
-                        //         NuevaFactura.subtotalventa = sum2
-                        //         NuevaFactura.iva = iva
-                        //             console.log("Tottal suma",TotalSuma);
-                        //             console.log("sum2" ,sum2);
-                        //             console.log("Tottal subventa",NuevaFactura.subtotalventa);
-                        //       // console.log("este variable suma0",suma);
-                        //         if (TableDetalleVenta != null) {
-                        //             document.getElementById("Subtotal").innerHTML = NuevaFactura.subtotalventa;
-                        //            document.getElementById("IVA").innerHTML = NuevaFactura.iva;
-                        //             document.getElementById("Total").innerHTML = NuevaFactura.totalventa;
-                        //             suma = 0;
-                        //         }
-                        //     })
+
+        this.TableDetalleVenta = new TableComponent({
+            ModelObject: new DetalleFactura(),
+            Dataset: this.DetallVenta,
+            Functions: [
+                {
+                    name: "eliminar", action: async (detaeli) => {
+                        const detalleelimina = this.DetallVenta.find(x => x.idtamanoxarticulo == detaeli.idtamanoxarticulo)
+                        if (detalleelimina != null) {
+                            // this.DetallVenta.splice(
+                            //     DetallVenta.indexOf(detaeli), 1);
+                            //     Total.splice(DetallVenta.indexOf(detaeli.totaldetalle),1);
+                            //     Total.forEach(function(total){
+                            //         var sum2=0
+                            //         suma +=total
+                            //        suma = suma + Total
+                            //         TotalSuma = Total.reduce((a, b) => Number(a) + Number(b), 0);
+                            //         iva = TotalSuma * 0.15;
+                            //         sum2 =TotalSuma
+                            //        totalventa = TotalSuma + iva
+                            //        ///
+                            //       NuevaFactura.totalventa = totalventa
+                            //         NuevaFactura.subtotalventa = sum2
+                            //         NuevaFactura.iva = iva
+                            //             console.log("Tottal suma",TotalSuma);
+                            //             console.log("sum2" ,sum2);
+                            //             console.log("Tottal subventa",NuevaFactura.subtotalventa);
+                            //       // console.log("este variable suma0",suma);
+                            //         if (TableDetalleVenta != null) {
+                            //             document.getElementById("Subtotal").innerHTML = NuevaFactura.subtotalventa;
+                            //            document.getElementById("IVA").innerHTML = NuevaFactura.iva;
+                            //             document.getElementById("Total").innerHTML = NuevaFactura.totalventa;
+                            //             suma = 0;
+                            //         }
+                            //     })
                             // Total.forEach(function(total){
                             //     suma += total;
                             //     //NuevaFactura.totalventa = Total
@@ -176,63 +186,63 @@ class Agregar extends HTMLElement {
                             //     console.log("totalventa",NuevaFactura.totalventa);
                             //     console.log("subtotal",NuevaFactura.subtotalventa);
                             //     console.log("suma",suma);
-                               
-                               
-                           // })
-                        //    if(DetallVenta[0] == null){
-                        //     console.log("este vacio 0");
-                        //     NuevaFactura.totalventa = 0
-                        //     NuevaFactura.subtotalventa = 0
-                        //     NuevaFactura.iva = 0
-                        //     console.log(NuevaFactura.totalventa);
-                        //     if (TableDetalleVenta != null) {
-                        //         document.getElementById("Subtotal").innerHTML = NuevaFactura.subtotalventa;
-                        //        document.getElementById("IVA").innerHTML = NuevaFactura.iva;
-                        //         document.getElementById("Total").innerHTML = NuevaFactura.totalventa;
-                        //         suma = 0;
-        
-                        //     }
-                        // }
-                        
+
+
+                            // })
+                            //    if(DetallVenta[0] == null){
+                            //     console.log("este vacio 0");
+                            //     NuevaFactura.totalventa = 0
+                            //     NuevaFactura.subtotalventa = 0
+                            //     NuevaFactura.iva = 0
+                            //     console.log(NuevaFactura.totalventa);
+                            //     if (TableDetalleVenta != null) {
+                            //         document.getElementById("Subtotal").innerHTML = NuevaFactura.subtotalventa;
+                            //        document.getElementById("IVA").innerHTML = NuevaFactura.iva;
+                            //         document.getElementById("Total").innerHTML = NuevaFactura.totalventa;
+                            //         suma = 0;
+
+                            //     }
+                            // }
+
+                        }
+                        TableDetalleVenta.DrawTableComponent();
+
                     }
-                    TableDetalleVenta.DrawTableComponent();
-                    
                 }
+            ]
+        });
+
+        this.TableDetalleVenta.filter.append(Render.Create({
+            tagName: 'input', type: 'button',
+            className: 'btnagregar', value: 'Anadir', onclick: async () => {
+                const Modal = new ModalComponent
+                    (new AgregarDetalleVenta((venta) => {
+                        if (this.DetallVenta.filter((x) => x.idtamanoxarticulo == venta.idtamanoxarticulo).length > 0) {
+                            alert("El Detalle ya existe")
+                            return;
+                        }
+                        this.DetallVenta.push(venta);
+                        // Total.push(venta.totaldetalle) //cada detalle
+                        // TotalSuma = Total.reduce((a, b) => Number(a) + Number(b), 0);
+                        // iva = TotalSuma * 0.15;
+                        // totalventa = TotalSuma + iva
+                        // // ConvertirMedida.push(DetalleCompra.ConvertirMedida);
+                        // console.log(venta.totaldetalle);
+                        // console.log(Total);
+                        // if (TableDetalleVenta != null) {
+                        //     document.getElementById("Subtotal").innerHTML = TotalSuma;
+                        //    document.getElementById("IVA").innerHTML = iva;
+                        //     document.getElementById("Total").innerHTML = totalventa;
+
+                        // }
+                        Modal.Close();
+                        this.TableDetalleVenta.DrawTableComponent();
+                    }))
+                AppMain.append(Modal);
             }
-        ]
-    });
 
-    this.TableDetalleVenta.filter.append(Render.Create({
-        tagName: 'input', type: 'button',
-        className: 'btnagregar', value: 'Anadir', onclick: async () => {
-            const Modal = new ModalComponent
-                (new AgregarDetalleVenta((venta) => {
-                    if (this.DetallVenta.filter((x) => x.idtamanoxarticulo == venta.idtamanoxarticulo).length > 0) {
-                        alert("El Detalle ya existe")
-                        return;
-                    }
-                    this.DetallVenta.push(venta);
-                    // Total.push(venta.totaldetalle) //cada detalle
-                    // TotalSuma = Total.reduce((a, b) => Number(a) + Number(b), 0);
-                    // iva = TotalSuma * 0.15;
-                    // totalventa = TotalSuma + iva
-                    // // ConvertirMedida.push(DetalleCompra.ConvertirMedida);
-                    // console.log(venta.totaldetalle);
-                    // console.log(Total);
-                    // if (TableDetalleVenta != null) {
-                    //     document.getElementById("Subtotal").innerHTML = TotalSuma;
-                    //    document.getElementById("IVA").innerHTML = iva;
-                    //     document.getElementById("Total").innerHTML = totalventa;
-
-                    // }
-                    Modal.Close();
-                    this.TableDetalleVenta.DrawTableComponent();
-                }))
-            AppMain.append(Modal);
-        }
-        
-    }))
-        this.append(this.Form,        (Render.Create({
+        }))
+        this.append(this.Form, (Render.Create({
             tagName: "div",
             innerHTML:
                 `  
@@ -253,9 +263,9 @@ class Agregar extends HTMLElement {
                 </tbody>
                 </table>
                 `
-    
+
         })),
-        this.Table, this.TableDetalleVenta);
+            this.Table, this.TableDetalleVenta);
         this.append(Render.Create({
             className: "FormContainer2",
             children: [
@@ -263,12 +273,12 @@ class Agregar extends HTMLElement {
                     tagName: "input",
                     type: "button",
                     className: "btn_primary",
-                            value: "Guardar",
+                    value: "Guardar",
                     onclick: async () => {
                         console.log(this.DetalleDevventa);
-                            // await AjaxTools.PostRequest("../api/GestionVenta/SaveDetalleDevolucionventa",
-                            // this.NewDevolucionVenta
-                            //  );
+                        // await AjaxTools.PostRequest("../api/GestionVenta/SaveDetalleDevolucionventa",
+                        // this.NewDevolucionVenta
+                        //  );
                         console.log(this.NewDevolucionVenta);
 
                     }
