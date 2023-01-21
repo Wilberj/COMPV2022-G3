c8528070107eb5f7f92bf6cae0f1a02104eb8b31
Kevinhuete <kevin.mhc25@gmail.com>
2023-01-21 14:19:20 -0600

    Mejoras

---
+++ Proyecto_LibreriaMA/CAPA_NEGOCIO/MODEL/articulodevuelto.cs
@@ -0,0 +1,48 @@
+﻿using CAPA_DATOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_NEGOCIO.MODEL
+{
+    public class InformeArticuloDevueltoEjemplo1
+    {
+
+        public string? nombrearticulo { get; set; }
+        //public DateTime? fechafactura { get; set;}
+        //public string? descripcionarticulo { get; set; }
+
+        // public int? iddetallefactura { get; set; }
+
+
+        //public int? idfactura { get; set; }
+        //public int? idarticulo { get; set; }
+        public Decimal? CantidadDevuelta { get; set; }
+        //public Decimal? TotalVenta { get; set; }
+
+        //public int? idtamanoxarticulo { get; set; }
+        //public Decimal? cantidadventa { get; set; }
+
+        public List<String>? Params { get; set; }
+
+        public Object TakeInformeArticuloDevuelto(InformeArticuloDevueltoEjemplo1 Inst)
+        {
+            try
+            {
+                List<Object> SqlParams = new List<Object>();
+                SqlParams.Add(Convert.ToDateTime(Params[0]));
+                SqlParams.Add(Convert.ToDateTime(Params[1]));
+                var Informe = SqlADOConexion.SQLM.TakeListWithProcedure<InformeArticuloDevueltoEjemplo1>(
+                    "usp_informeArticuloMasDevuelto", Inst, SqlParams);
+                return Informe;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Proyecto_LibreriaMA/UI/Controllers/InformesArticuloDevueltoController.cs
@@ -0,0 +1,22 @@
+﻿using CAPA_NEGOCIO.MODEL;
+using CAPA_NEGOCIO.SECURITY;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace UI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InformesArticuloDevueltoController : Controller
+    {
+       public InformesArticuloDevueltoController()
+        {
+            AuthNetCore.loginIN("sa", "1234");
+        }
+        [HttpPost]
+        public object TakeInformeArticuloDevueltoEjemplo1(InformeArticuloDevueltoEjemplo1 ObjInst)
+        {
+            return ObjInst.TakeInformeArticuloDevuelto(ObjInst);
+        }
+    }
+}
